24724f8d11dbc86b9f6b3d14c9996f53dd1fe4d8
Juan Urdaneta <jurdanetac10@gmail.com>
2024-05-03 12:03:20 -0400

    Fix npm scripts

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: "airbnb-base",
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
     },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
-  rules: {
-    "no-console": "off",
-    "no-underscore-dangle": "off",
-    "no-param-reassign": ["error", { props: false }],
-  },
-};
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-undef": "off"
+    }
+}
